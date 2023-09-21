8bde37136300c5795f81c19978f0534a02789587
ppedvLukasK <lukask@ppedv.de>
2023-09-21 15:51:38 +0200

    M010

---
+++ M010/IArbeit.cs
@@ -0,0 +1,26 @@
+﻿namespace M010;
+
+/// <summary>
+/// Interface: Struktur, die ihre Inhalte an die Unterklassen erzwingt
+/// Hier werden Properties und Methoden ohne Body definiert, die in den Unterklassen implementiert werden müssen
+/// Kann nicht instanziert werden
+/// </summary>
+public interface IArbeit
+{
+	static readonly int Wochenstunden = 40; //Konstante
+
+	string Job { get; set; }
+
+	int Gehalt { get; set; }
+
+	void Lohnauszahlung();
+
+	int Jahresgehalt();
+
+	int LohnProStunde(int lohn);
+
+	void Test()
+	{
+		//Bad Practice, Interfaces sind nur für die Struktur da
+	}
+}
---
+++ M010/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace M010;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.OutputEncoding = Encoding.UTF8;
+
+		Mensch m = new Mensch();
+		m.Job = "Softwareentwickler";
+		m.Gehalt = 5000;
+
+		m.Lohnauszahlung();
+
+		//Polymorphismus
+		//-> Interfaces sollen den implementierenden Klassen einen extra Typen geben
+		IArbeit arbeit = m;
+		arbeit.Lohnauszahlung();
+
+		arbeit = new Mensch2();
+		arbeit.Lohnauszahlung();
+
+		//Beispiele für vorhandene Interfaces
+		//IEnumerable: Basis von allen Listentypen in C#
+		int[] x = new int[10];
+		List<int> y = new List<int>();
+		Dictionary<int, int> z = new Dictionary<int, int>();
+		//Diese 3 Listentypen haben alle das IEnumerable Interface
+		PrintList(x);
+		PrintList(y);
+		PrintList(z); //Hier passen alle 3 Listen hinein
+	}
+
+	public static void PrintList<T>(IEnumerable<T> a)
+	{
+
+	}
+}
+
+public class Mensch : IArbeit
+{
+	public string Job { get; set; }
+
+	public int Gehalt { get; set; }
+
+	public int Jahresgehalt()
+	{
+		return Gehalt * 12;
+	}
+
+	public void Lohnauszahlung()
+	{
+        Console.WriteLine($"Diese Person hat für den Job {Job} ein Gehalt von {Gehalt}€ bekommen." +
+			$"Er arbeitet {IArbeit.Wochenstunden} Stunden pro Woche.");
+    }
+
+	public int LohnProStunde(int lohn)
+	{
+		return lohn / 160;
+	}
+}
+
+public class Mensch2 : IArbeit
+{
+	public string Job { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	public int Gehalt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+	public int Jahresgehalt()
+	{
+		throw new NotImplementedException();
+	}
+
+	public void Lohnauszahlung()
+	{
+
+	}
+
+	public int LohnProStunde(int lohn)
+	{
+		throw new NotImplementedException();
+	}
+}
